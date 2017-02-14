7f8d26757cea52eface46ef5fd3f15ffea39b99b
Yitian Zhang <skyler2zhang@gmail.com>
2017-02-14 03:34:40 -0800

    script for text UI
    get the score of the player class and show it in the ui component

---
+++ ScoreControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreControl : MonoBehaviour {
+
+	float score;
+	public Text showscore; //the number showed in the UI
+	public Control player; //to get the player's score
+
+	// Use this for initialization
+	void Start () {
+		score = 0;	//set the original score to be 0
+		showscore.text = score.ToString ();
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		score = player.getscore(); 	//getscore() is a public function of the player class 
+		showscore.text = score.ToString();
+	}
+}
