db05da35f84314f47adef642193735e3ef027a9e
Niccolo Pucci <npucci@sfu.ca>
2017-02-14 14:23:57 -0800

    Created separate script for ant collision detection with items

---
+++ AntCollider.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AntCollider : MonoBehaviour {
+	private PlayerController player;
+
+	void Start() {
+		player = GameObject.Find("Player").GetComponent<PlayerController>();;
+	}
+		
+	private void OnCollisionEnter2D(Collision2D Coll)
+	{
+		if (Coll.gameObject.name.Contains("Item")) {
+			player.incrementScore ();
+			Destroy (Coll.gameObject);
+		} 
+	}
+}
