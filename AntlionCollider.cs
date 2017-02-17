0a2c1cdf2e6b656e8b8dfec9868bccad9fd578f0
Niccolo Pucci <npucci@sfu.ca>
2017-02-16 22:18:01 -0800

    Added complete Fire Ant mechanics

---
+++ AntlionCollider.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AntlionCollider : MonoBehaviour {
+	private AntlionBehavior antlion;
+	private Collider2D blockingObstacleColl;
+	private GameObject player;
+	void Start() {
+		antlion = GameObject.Find("Antlion").GetComponent<AntlionBehavior>();
+		player = GameObject.Find ("Player").GetComponent<Transform> ().gameObject;
+		blockingObstacleColl = null;
+	}
+
+	private void OnCollisionEnter2D(Collision2D coll) {
+		bool gameOver = antlion.isGameOver (); 
+		if (!gameOver && coll.gameObject.name.Contains ("Ant")) {
+			if (coll.gameObject.name.Contains ("Fire Ant") && player.GetComponent<PlayerController> ().holdingThrowableObject ()) {
+				player.GetComponent<PlayerController> ().destroyThrowableObject ();
+				antlion.setEating (true);
+			}
+
+			else {
+				GameObject ant = player.GetComponent<PlayerController> ().lastAntEaten (coll.gameObject);
+				Destroy (ant);
+				antlion.setEating (true);
+			}
+		} 
+	
+		else if (!gameOver && coll.gameObject.name.Contains ("Throwable Obstacle")) {
+			if (coll.gameObject.GetComponent<Rigidbody2D> ().velocity.x < 0) {
+				antlion.setEating (true);
+			}
+			Destroy (coll.gameObject);
+		}
+	}
+}
