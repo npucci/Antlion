f32600c931a51c7298d1017ed889fe8069b38a77
Niccolo Pucci <npucci@sfu.ca>
2017-02-18 06:32:51 -0800

    Moved control logic to level manager, Moved scoring system solely to score control (no longer associated with player)

--- LevelManager.cs
+++ LevelManager.cs
@@ -9,9 +9,7 @@ public class LevelManager : MonoBehaviour {
 	private GameObject playerEndNode;
 	private GameObject player;
 	private GameObject antlion;
-	private bool pause = false;
 
-	// Use this for initialization
 	void Start () {
 		antlionStartNode = transform.GetChild (0).gameObject;
 		antlionEndNode = transform.GetChild (1).gameObject;
@@ -27,36 +25,25 @@ public class LevelManager : MonoBehaviour {
 		player.GetComponent<PlayerController> ().startMovement ();
 		antlion.GetComponent<AntlionBehavior> ().startMovement ();
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
-		if (Input.GetKeyDown (KeyCode.Escape)) {
-			pause = !pause;
-		}
-
-		if (pause) {
-			pauseGame ();
-		} 
-		else {
-			resumeGame ();
-		}
-
+		// Win
 		if (player.transform.position.x >= playerEndNode.transform.position.x) {
-			Debug.Log ("You Win!");
-			player.GetComponent<PlayerController>().stopMovement ();
-			antlion.GetComponent<AntlionBehavior>().stopMovement ();
+			GameObject.Find("Controller Canvas").GetComponent<CanvasController>().win();
+			player.GetComponent<PlayerController> ().stopMovement ();
+			antlion.GetComponent<AntlionBehavior> ().stopMovement ();
 		}
 
+		// Antlion Stops, leading to Win
 		if (antlion.transform.position.x >= antlionEndNode.transform.position.x) {
 			antlion.GetComponent<AntlionBehavior>().stopMovement ();
 		}
-	}
 
-	private void pauseGame() {
-		Time.timeScale = 0f;
-	}
-
-	private void resumeGame() {
-		Time.timeScale = 1f;
+		// Game Over
+		if (player.GetComponent<PlayerController> ().allAntsEaten ()) {
+			GameObject.Find("Controller Canvas").GetComponent<CanvasController>().gameOver();
+			player.GetComponent<PlayerController>().stopMovement ();
+			antlion.GetComponent<AntlionBehavior>().stopMovement ();
+		}
 	}
 }
--- ScoreControl.cs
+++ ScoreControl.cs
@@ -4,28 +4,109 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class ScoreControl : MonoBehaviour {
-	private float score;
-	private PlayerController player; //to get the player's score
+	private int score;
 
-	public Text showscore; //the number showed in the UI
+	private int speedBoostsAvailable = 0; // power up
+	private float speedBoost = 2.0f;
+	private float boostTimer = 2.0f; // times power up
+	private float boostCountDown = 0f; // stores current timing of timer
+
+	private int scoreMultiplier = 2; // power up
+	private int currentScoreMultiplier = 1;
+	private float multiplierTimer = 2f; // times power up
+	private float multiplierCountDown = 0f; // stores current timing of timer
+
+	public Text hudShowScore; //the number showed in the UI HUD Canvas
+	public Text hudShowMultiplier;
+	public Text hudShowBoosts;
+	public Text pauseShowScore; //the number showed in the UI Pause Canvas
+	public Text pauseShowMultiplier;
+	public Text pauseShowBoosts;
 
 	// Use this for initialization
 	void Start () {
-		player = GameObject.Find ("Player").GetComponent<PlayerController> ();
 		score = 0;	//set the original score to be 0
-		showscore.text = "Score: " + score.ToString ();
-
+		updateTexts();
 	}
 
 	// Update is called once per frame
 	void Update () {
-		if (player != null) {
-			score = player.getscore (); 	//getscore() is a public function of the player class 
-			showscore.text = "Score: " + score.ToString ();
+		updateTexts();
+	
+		if (boostCountDown > 0) {
+			boostCountDown -= Time.deltaTime;
 		}
 
-		if(player.GetComponent<PlayerController>().allAntsEaten()) {
-			// GAME OVER
+		if (multiplierCountDown > 0) {
+			multiplierCountDown -= Time.deltaTime;
+		} 
+
+		else {
+			currentScoreMultiplier = 1;
 		}
+	}
+
+	public void incrementScore(string itemName) {
+		// apple == 3 points
+		if (itemName.Contains ("Apple Item")) {
+			score += 3 * currentScoreMultiplier;
+		} 
+
+		// strawberry == 2 points
+		else if (itemName.Contains ("Strawberry Item")) {
+			score += 2 * currentScoreMultiplier;
+		} 
+
+		// blueberry == 1 point
+		else if (itemName.Contains ("Blueberry Item")) {
+			score += 1 * currentScoreMultiplier;
+		} 
+
+		// sugar cube == 2x multiplier (compounding)
+		else if (itemName.Contains ("Sugar Cube Item")) {
+			// if timer is not counting down, set it and start
+			if (multiplierCountDown <= 0) {
+				multiplierCountDown = multiplierTimer;
+			}
+			currentScoreMultiplier *= scoreMultiplier;
+		}
+
+		// nectar drop == 1 speed boost (time accumulative)
+		else if (itemName.Contains ("Nectar Drop Item")) {
+			speedBoostsAvailable += 1;
+		}
+	}
+
+	public float getBoost(bool request) {
+		// if timer is currently going
+		if (boostCountDown > 0) {
+			return speedBoost;
+		} 
+
+		// if player requests a boost, and there is one available
+		else if (request && speedBoostsAvailable > 0) {
+			speedBoostsAvailable -= 1;
+			boostCountDown = boostTimer;
+			return speedBoost;
+		}
+
+		// default boost
+		else {
+			return 1f;
+		}
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	private void updateTexts() {
+		hudShowScore.text = score.ToString ();
+		hudShowMultiplier.text = "(x" + currentScoreMultiplier.ToString () + ")";
+		hudShowBoosts.text = speedBoostsAvailable.ToString ();
+
+		pauseShowScore.text = score.ToString ();
+		pauseShowMultiplier.text = "(x" + currentScoreMultiplier.ToString () + ")";
+		pauseShowBoosts.text = speedBoostsAvailable.ToString ();
 	}
 }
